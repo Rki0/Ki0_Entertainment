a27d44c4f7abaf191909f6717f5d543a8228376a
Rki0 <pky00823@gmail.com>
2022-08-26 01:18:23 +0900

    adding effect to artsitList

--- src/pages/ArtistPage/ArtistsList.tsx
+++ src/pages/ArtistPage/ArtistsList.tsx
@@ -1,17 +1,28 @@
 import { artistArr, ArtistsListPropsType } from "./Interface";
 
 function ArtistsList({ moveToArtist, currArtist }: ArtistsListPropsType) {
-  console.log(currArtist);
+  // 스크롤에 따라 currArtist가 변경됨. 0,1,2,3,4
+  // 해당 값(인덱스)에 있는 artist만 black으로 처리하고
+  // 나머지는 불투명 처리
+  // 방법
+  // currArtist와 artistArr의 index를 비교하자
 
   return (
     <nav className="hidden lg:flex justify-center w-screen top-1/2 fixed">
       <ul className="flex flex-col items-center font-semibold xl:text-xl">
         {artistArr.map((item, index) => (
           <li
-            className="hover:cursor-pointer first:border-b-2 border-black"
-            // className={`hover:cursor-pointer [&:nth-child(${
-            //   currArtist + 1
-            // })]:border-b-2 border-black`}
+            // className={
+            //   currArtist === index
+            //     ? "hover:cursor-pointer font-bold text-black"
+            //     : "hover:cursor-pointer text-gray-400"
+            // }
+            className={
+              `hover:cursor-pointer transition-all duration-300 ease-in-out` +
+              `${
+                currArtist === index ? "font-bold text-black" : " text-gray-400"
+              }`
+            }
             key={index}
             onClick={() => moveToArtist(index)}
           >
