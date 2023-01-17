0a1b887432b6401b8cac09764326d18882398e61
Rki0 <pky00823@gmail.com>
2023-01-17 22:40:32 +0900

    chore(all): modify package dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
-  // calc를 사용할 수 있게 만들어준다.
-  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
-
-// module.exports = {
-//   content: ["./src/**/*.{html,js,ts,tsx}", "./public/index.html"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
